c24a053452cdbbf737c77f5965624adcc8efcea4
Joshua Steier <josteier@optonline.net>
2016-08-13 16:49:14 -0500

    Update ps0.py

--- ps0.py
+++ ps0.py
@@ -1,7 +1,8 @@
-#Problem Set 0
+#Problem Set 0- Fall 2008 ,6.00 MIT
 #Name: Joshua Steier
 #Collaborators: None
 #Time: 1:30
+#The program will prompt the user for first name and last name, and returns them on the screen.
 x= raw_input("What's your first name?")
 y= raw_input("What's your last name?")
 print("your first name is:\n" + x)
