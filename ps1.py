bbe22d7e35136ccf673639bafec90ca6c6f297da
Joshua Steier <josteier@optonline.net>
2016-08-13 16:46:14 -0500

    Update ps1.py

--- ps1.py
+++ ps1.py
@@ -1,7 +1,8 @@
-#Problem Set 1: Problem 1, First try, note output doesn't work correctly
+#Problem Set 1: Problem 1, First try, note output doesn't work correctly, as per edX course, part of my semi- mit challenge
 #Author: Joshua Steier
 #Collaborators: None
 #Time: 0:40
+#The program will prompt user for a string, the string will count and print out the number of vowels
 s= raw_input("Enter in a string: \n")
 count = 0
 for letter in s:
