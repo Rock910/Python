b29709a39aa1c4876ccbef4539db2675cf4e829e
Joshua Steier <josteier@optonline.net>
2016-08-12 23:28:56 -0400

    Add files via upload
    First problem set, problem set 0, MIT's 6.00 from Fall 2008

---
+++ ps0.py
@@ -0,0 +1,8 @@
+#Problem Set 0
+#Name: Joshua Steier
+#Collaborators: None
+#Time: 1:30
+x= raw_input("What's your first name?")
+y= raw_input("What's your last name?")
+print("your first name is:\n" + x)
+print("your last name is:\n" + y)
