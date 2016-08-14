0f3103ead9262c92ddb6aba8a70663c94fbe30fd
Joshua Steier <josteier@optonline.net>
2016-08-13 21:10:30 -0400

    Add files via upload

---
+++ evenorodd.py
@@ -0,0 +1,10 @@
+#Program will print out if a number is even or odd
+#Author: Joshua Steier
+#Time: 0:30 Seconds
+x= int(raw_input("Enter in an integer: \n"))
+if x % 2 == 0:
+    print "Even\n"
+elif x ==0:
+    print "Zero\n"
+else:
+    print "Odd\n"
