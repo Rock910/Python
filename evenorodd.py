d0bd19fa30f10a1dbac2635ef44966e66a424c4d
Joshua Steier <josteier@optonline.net>
2016-08-13 20:12:17 -0500

    Update evenorodd.py

--- evenorodd.py
+++ evenorodd.py
@@ -2,9 +2,9 @@
 #Author: Joshua Steier
 #Time: 0:30 Seconds
 x= int(raw_input("Enter in an integer: \n"))
-if x % 2 == 0:
+if x % 2 == 0 and x > 0:
     print "Even\n"
-elif x ==0:
+elif x == 0:
     print "Zero\n"
 else:
     print "Odd\n"
