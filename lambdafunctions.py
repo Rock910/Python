358c7d6d8121157d92af02e7283c3717dded304c
Joshua Steier <josteier@optonline.net>
2016-11-01 09:40:04 -0500

    Add files via upload

---
+++ lambdafunctions.py
@@ -0,0 +1,21 @@
+#Practice with making lambda functions
+#Author: Joshua Steier
+#Date 11/1/2016
+#First I'll define the sum of two values
+summation = lambda x, y: x + y
+#subtraction of two values
+subtraction = lambda x, y: x -y
+#multiplication
+multiplication = lambda x, y: x * y
+#division 
+division= lambda x,y : x / y
+#the square of a number
+square= lambda x: x **2
+#the cube of a number
+cube= lambda x: x**3
+#perhaps a formula a^2 + b^2 = c^2
+csquared= lambda a, b: a **2 + b**2
+#square root of a number
+sqroot= lambda a: a ** 0.5
+#average of numbers
+average= lambda a: sum(a) / len(a)
