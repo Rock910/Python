f56f035f86dd75716dc7df7f82ea7943be776268
Joshua Steier <josteier@optonline.net>
2017-03-07 21:51:46 -0500

    Add files via upload

---
+++ atmMachine.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  7 21:51:02 2017
+
+@author: steierjo
+"""
+
+#ATM machine directions: use the account class to simulate an atm machine.
+#Createten accounts, in a list with ids 0 to 9, and balance of 100
+#System prompts user for an id, if incorrect, we ask for a correction, 
+#Once accepted the main menu is made, choice 1 is balance
+#2 is withdraw
+#3 deposit
+#4 exit
